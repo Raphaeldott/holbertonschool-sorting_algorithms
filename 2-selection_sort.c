8eee2349be58690393714bf625cb245b5e283e75
raphaeldott <raphael.dott@hotmail.com>
2024-12-13 22:56:53 +0100

    task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -3,31 +3,31 @@
 #include "sort.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
+ * selection_sort - Sorts an array of integers in ascending order
+ * using the Selection Sort algorithm.
+ * @array: An array of integers
+ * @size: The size of the array
  */
-
 void selection_sort(int *array, size_t size)
 {
-	size_t a, b, mini;
-	int temp;
+	size_t a, b, mini, temp;
 
-
-	for (a = 0; a < size - 1; a++)
+	for (a = 0; a < size; a++)
 	{
 		mini = a;
 
-		for (b = 0; b < size; b++)
+		for (b = a + 1; b < size; b++)
 		{
 			if (array[b] < array[mini])
-			{
 				mini = b;
-				temp = array[a];
-				array[a] = array[b];
-				array[b] = temp;
-			}
-}
+		}
+
+		if (mini != a)
+		{
+			temp = array[a];
+			array[a] = array[mini];
+			array[mini] = temp;
 			print_array(array, size);
+		}
 	}
 }
